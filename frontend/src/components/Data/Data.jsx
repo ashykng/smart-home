63852570d0150f30cdd3edcd0fb85c522c6ddb16
Ashkan Tavassoli <ashkant939@gmail.com>
2025-05-01 08:31:00 +0330

    🔥 Add: added codes

--- frontend/src/components/Data/Data.jsx
+++ frontend/src/components/Data/Data.jsx
@@ -1,10 +1,44 @@
 import { CardDefault } from '../general/card/card'
 import axios from 'axios'
 import { endpoints } from '../../../config'
+import { API_URL } from '../../../config'
 import { useEffect, useState } from 'react'
+
+import { HiSpeakerWave } from "react-icons/hi2"
+import { FaLightbulb, FaTemperatureHigh } from "react-icons/fa"
+import { FaKeyboard } from 'react-icons/fa6'
+import { WiHumidity } from "react-icons/wi"
+import { LuHeater } from "react-icons/lu"
 
 export default function Data() {
     const [data, setData] = useState({})
+
+    const cardConfig = {
+        'buzzer': {
+            toggleApi: `${API_URL}/toggle/buzzer`,
+            icon: <HiSpeakerWave className="text-gray-600 w-5 h-5" />
+        },
+        'light': {
+            toggleApi: `${API_URL}/toggle/light`,
+            icon: <FaLightbulb className="text-yellow-500 w-5 h-5" />
+        },
+        'heater': {
+            toggleApi: `${API_URL}/toggle/heater`,
+            icon: <LuHeater className="text-red-500 w-5 h-5" />
+        },
+        'light Level': {
+            icon: <FaLightbulb className="text-blue-500 w-5 h-5" />
+        },
+        'temperature': {
+            icon: <FaTemperatureHigh className="text-red-500 w-5 h-5" />
+        },
+        'pressed Key': {
+            icon: <FaKeyboard className="text-blue-500 w-5 h-5" />
+        },
+        'humidity': {
+            icon: <WiHumidity className="text-blue-500 w-6 h-6" />
+        }
+    }
 
     async function fetchData() {
         try {
@@ -22,18 +56,22 @@ export default function Data() {
         }, 500)
 
         return () => clearInterval(interval)
-    }, []);
+    }, [])
 
     return (
-        <div className='p-2'>
-            {data && Object.entries(data).map(([key, value]) => (
-                <CardDefault
-                    key={key}
-                    title={key.toUpperCase()}
-                    data={value}
-                    toggleApi={key == "buzzer" ? "test" : null}
-                />
-            ))}
+        <div style={{ display: "ruby" }}>
+            {data && Object.entries(data).map(([key, value]) => {
+                const config = cardConfig[key] || {}
+                return (
+                    <CardDefault
+                        key={key}
+                        title={key.toUpperCase()}
+                        data={value}
+                        toggleApi={config.toggleApi || null}
+                        icon={config.icon || null}
+                    />
+                )
+            })}
         </div>
-    );
+    )
 }
--- frontend/src/components/general/Card/Card.jsx
+++ frontend/src/components/general/Card/Card.jsx
@@ -1,14 +1,31 @@
-import { Card, CardBody, Typography } from "@material-tailwind/react";
+import { Card, CardBody, Typography } from "@material-tailwind/react"
+import axios from 'axios'
+
+async function toggle(toggleApi) {
+    try {
+        const response = await axios.get(toggleApi)
+        console.log(response.data)
+    } catch (error) {
+        console.error('Error fetching data:', error)
+    }
+}
 
 export function CardDefault({
     title,
     data,
-    toggleApi
+    toggleApi,
+    icon
 }) {
     return (
-        <Card className="mt-6 w-80 shadow-lg rounded-2xl border border-gray-200">
+        <Card className="mx-4 mt-6 w-80 shadow-lg rounded-2xl border border-gray-200">
             <CardBody className="flex flex-col gap-4 p-4">
                 <div className="flex items-center justify-between">
+                    {icon && (
+                        <div className="flex items-center pr-4 border-r border-gray-300">
+                            {icon}
+                        </div>
+                    )}
+
                     <Typography variant="h5" color="blue-gray" className="font-semibold">
                         {title}
                     </Typography>
@@ -16,10 +33,10 @@ export function CardDefault({
                     {typeof data === "boolean" ? (
                         <button
                             type="button"
-                            onClick={toggleApi}
+                            onClick={() => toggle(toggleApi)}
                             className="px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-md hover:bg-blue-600 transition cursor-pointer"
                         >
-                            Toggle
+                            {data.toString()} | Toggle
                         </button>
                     ) : (
                         <Typography color="gray" className="text-sm font-medium">
